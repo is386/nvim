38c420ee1c43d313e9b75776b4601b81455634fc
is386 <20195347+is386@users.noreply.github.com>
2026-03-09 12:27:36 -0400

    fix lualine performance

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,7 +1,30 @@
+local git_root_cache = {}
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'DirChanged' }, {
+  callback = function()
+    git_root_cache[vim.api.nvim_get_current_buf()] = nil
+  end,
+})
+
+local function project_name()
+  local buf = vim.api.nvim_get_current_buf()
+
+  if not git_root_cache[buf] then
+    local root = vim.fn.system('git rev-parse --show-toplevel 2>/dev/null'):gsub('\n', '')
+    local dir = (vim.v.shell_error == 0 and root ~= '') and root or vim.fn.getcwd()
+    git_root_cache[buf] = vim.fn.fnamemodify(dir, ':t')
+  end
+
+  return git_root_cache[buf]
+end
+
+local function clock()
+  return os.date '%I:%M'
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = {},
     opts = {
       options = {
         theme = 'nord',
@@ -10,18 +33,11 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = {
-          function()
-            local root = vim.fn.system('git rev-parse --show-toplevel 2>/dev/null'):gsub('\n', '')
-            if vim.v.shell_error == 0 and root ~= '' then return vim.fn.fnamemodify(root, ':t') end
-            return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-          end,
-          { 'branch' },
-        },
-        lualine_d = { { 'filename', symbols = { modified = ' ●' } } },
+        lualine_b = { project_name, 'branch' },
+        lualine_c = { { 'filename', symbols = { modified = ' ●' } } },
         lualine_x = {},
         lualine_y = {},
-        lualine_z = { function() return os.date '%I:%M' end },
+        lualine_z = { clock },
       },
       inactive_sections = {
         lualine_a = {},
