f73f77ade5ac6d9d0b0745c6d4288ee829065de4
is386 <20195347+is386@users.noreply.github.com>
2026-03-11 01:00:02 -0400

    add harpoon and split keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,12 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set('n', '<leader>dd', '<cmd>split<CR>', { desc = 'Down' })
+vim.keymap.set('n', '<leader>dr', '<cmd>vsplit<CR>', { desc = 'Right' })
+vim.keymap.set('n', '<leader>dw', '<cmd>close<CR>', { desc = 'Close' })
+
+vim.keymap.set('n', '<leader><Left>', '<C-w>h', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader><Down>', '<C-w>j', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader><Up>', '<C-w>k', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader><Right>', '<C-w>l', { desc = 'which_key_ignore' })
--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,14 +3,6 @@ return {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>cf',
-        function() require('conform').format { async = true, lsp_format = 'fallback' } end,
-        mode = '',
-        desc = 'Format',
-      },
-    },
     ---@module 'conform'
     ---@type conform.setupOpts
     opts = {
