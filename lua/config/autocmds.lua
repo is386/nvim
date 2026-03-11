ec64628d55e4ba6446b7d170f4c7a247aca39438
is386 <20195347+is386@users.noreply.github.com>
2026-03-10 20:18:16 -0400

    add vim leave auto cmd to reset cursor style

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+vim.api.nvim_create_autocmd('VimLeave', {
+  desc = 'Restore terminal cursor on exit',
+  callback = function() vim.opt.guicursor = 'a:ver1-blinkon1' end,
+})
