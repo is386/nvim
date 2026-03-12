cfa92d3535f16c4fb6d5faea1d0883a4d12f013a
is386 <20195347+is386@users.noreply.github.com>
2026-03-11 22:46:21 -0400

    add some useful keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,9 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR><cmd>w<CR>')
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('n', 'H', '^', { desc = 'Go to first non-blank character' })
+vim.keymap.set('n', 'L', '$', { desc = 'Go to end of line' })
 
 vim.keymap.set('n', '<leader>dd', '<cmd>belowright split<CR>', { desc = 'Down' })
 vim.keymap.set('n', '<leader>dr', '<cmd>belowright vsplit<CR>', { desc = 'Right' })
@@ -9,7 +11,7 @@ vim.keymap.set('n', '<leader>du', '<cmd>above split<CR>', { desc = 'Up' })
 vim.keymap.set('n', '<leader>dl', '<cmd>leftabove vsplit<CR>', { desc = 'Left' })
 vim.keymap.set('n', '<leader>w', '<cmd>close<CR>', { desc = 'Close' })
 
-vim.keymap.set('n', '<leader><Left>', '<C-w>h', { desc = 'which_key_ignore' })
-vim.keymap.set('n', '<leader><Down>', '<C-w>j', { desc = 'which_key_ignore' })
-vim.keymap.set('n', '<leader><Up>', '<C-w>k', { desc = 'which_key_ignore' })
-vim.keymap.set('n', '<leader><Right>', '<C-w>l', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader>h', '<C-w>h', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { desc = 'which_key_ignore' })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { desc = 'which_key_ignore' })
