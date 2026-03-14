d0ec3a77b60900d6e4bc847699a21a354f3c0c31
is386 <20195347+is386@users.noreply.github.com>
2026-03-13 23:08:17 -0400

    add golang support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ':TSUpdate',
     branch = 'main',
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'javascript', 'jsdoc', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'tsx', 'typescript', 'vim', 'vimdoc' }
+      local parsers = { 'bash', 'c', 'diff', 'go', 'gomod', 'gosum', 'gotmpl', 'html', 'javascript', 'jsdoc', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'rust', 'toml', 'tsx', 'typescript', 'vim', 'vimdoc' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
